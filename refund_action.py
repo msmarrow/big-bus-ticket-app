d9d825daaf2c98d78210c6d4be864a29303f3589
Mahjeed Marrow <msmarrow@cs.uchicago.edu>
2019-05-07 13:56:09 -0500

    refactor

--- refund_action.py
+++ refund_action.py
@@ -6,6 +6,5 @@ import ticket as Tix
 import util
 
 def issue_refund(ticket_id, route_ledger, ticket_records):
-    count = 0
     updated_records = Tix.remove_ticket_from_records(ticket_id, ticket_records, route_ledger)
     return updated_records
