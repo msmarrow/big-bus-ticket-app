3301369ffea628cd95456158a8eaaa1468f8b7a6
Mahjeed Marrow <msmarrow@cs.uchicago.edu>
2019-05-06 19:16:46 -0500

    green

--- big_bus_test.py
+++ big_bus_test.py
@@ -1,14 +1,14 @@
 import unittest
 import cmd
-import big_bus as bus
-import buy_action as buy
+import big_bus as Bus
+import buy_action as Buy
 
 
 class BigBusTest(unittest.TestCase):
     '''unit testing for big bus ticket purchase software'''
     def test_bought_valid_number_of_tickets(self):
-        self.assertEqual(buy.is_valid_number_of_tickets(5), False)
-        self.assertEqual(buy.is_valid_number_of_tickets(4), True)
+        self.assertEqual(Buy.is_valid_number_of_tickets(5), False)
+        self.assertEqual(Buy.is_valid_number_of_tickets(4), True)
 
     def test_did_input_valid_route(self):
         blue_string = "blue"
@@ -16,9 +16,9 @@ class BigBusTest(unittest.TestCase):
         green_string = "greEn"
         purple_string = "purple"
 
-        self.assertEqual(is_valid_route(blue_string), True)
-        self.assertEqual(is_valid_route(red_string), True)
-        self.assertEqual(is_valid_route(green_string), True)
-        self.assertEqual(is_valid_route(purple_string), False)
+        self.assertEqual(Buy.is_valid_route(blue_string), True)
+        self.assertEqual(Buy.is_valid_route(red_string), True)
+        self.assertEqual(Buy.is_valid_route(green_string), True)
+        self.assertEqual(Buy.is_valid_route(purple_string), False)
 
 unittest.main()
--- buy_action.py
+++ buy_action.py
@@ -5,5 +5,14 @@ def is_valid_number_of_tickets(ticket_order_number):
     else:
         return False
 
-def get_bus_route():
+def get_bus_route_request():
     return input("Select a route: Red, Green, or Blue: ")
+
+def is_valid_route(route_request):
+    route_lower_case = route_request.lower()
+    valid_routes = ["red","green","blue"]
+
+    if route_lower_case in valid_routes:
+        return True
+    else:
+        return False
